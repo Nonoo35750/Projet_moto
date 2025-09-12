c7177c0c97dbde55202e274ccc14c013fc873cfb
loicbihan <loicbihan5635@gmail.com>
2025-09-12 10:21:37 +0200

    MOdif motot cpp h

--- Moto.h
+++ Moto.h
@@ -3,14 +3,25 @@
  * \author LOIC
  * \date september 2k25
  *********************************************************************/
+#include <iostream>
+#include <string>
+
 class Moto
 {
 private:
+   
+    int Poids;
+    std::string moteur;
 
 protected:
 
 public:
+    /**
+        * Mise en forme du constructeur/destructeur
+        */
     Moto();
+
+    void print();
 
     ~Moto();
 
