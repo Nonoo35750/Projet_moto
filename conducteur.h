0af7154e2389aa44c945f074b49e47a979c88941
Nonoo35750 <nolangageot912@gmail.com>
2025-09-12 10:04:47 +0200

    ajout du nouveau main et des nouveau fichiers conducteur

--- conducteur.h
+++ conducteur.h
@@ -1,7 +1,18 @@
-class Conducteur {
-		private :
+#include <string>
+#include <iostream>
 
+class Conducteur {
+		protected:
+
+		private :
+			std::string nom;
 		public :
 			Conducteur();
-		    Conducteur	~Conducteur()
+			~Conducteur();
+			std::string getNomConducteur();
+			void setEntreprise(std::string nouveauNom);
+			std::string getPrenomConducteur();
+			void setPrenomConducteur(std::string NouveauPrenom);
+	
+			std::string getNom();	
 };
